385b9f5f9b0d7fabd61ad0ce00df413aa66e26c6
Mark Farrell <Markpeterfarrell@gmail.com>
2023-06-15 15:57:23 +0100

    Front-end, JS

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,56 @@
+import { InputForm } from "@dfinity/candid";
 import { dbank_backend } from "../../declarations/dbank_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+window.addEventListener("load", async function(){
 
-  button.setAttribute("disabled", true);
+const currentAmount = await dbank_backend.checkBalance();
+var roundedAmount = Math.round(currentAmount * 100) / 100
+document.getElementById("value").innerText = roundedAmount;
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
 
-  button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
 
-  return false;
+
+console.log(dbank_backend.checkBalance());
+
 });
+
+window.addEventListener("submit", async function(event){
+
+event.preventDefault();
+
+const depositAmount = parseFloat(document.getElementById("deposit-amount").value);
+const withdrawAmount = parseFloat(document.getElementById("withdraw-amount").value);
+console.log(depositAmount);
+
+
+
+
+
+  if(depositAmount > 0) {
+
+     dbank_backend.topUp(depositAmount);
+    console.log("deposit triggered " + depositAmount)
+
+
+
+  } else if (withdrawAmount > 0) {
+
+
+    console.log("withdraw triggered " + withdrawAmount)
+     dbank_backend.withdraw(withdrawAmount);
+
+ 
+
+  }
+
+ 
+});
+
+// dbank_backend.checkBalance
+
+
+
+
+
